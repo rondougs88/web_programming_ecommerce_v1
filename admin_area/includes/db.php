519ff63888c1facebb34d3a49174997bc2fadf9f
Ron <rondougs88@gmail.com>
2019-04-05 13:11:17 +1300

    changed db server to www.db4free.net

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,7 +1,8 @@
 <?php 
 // After uploading to online server, change this connection accordingly
 
-$con = mysqli_connect("localhost","root","","assignment2_v1");
+// $con = mysqli_connect("localhost","root","","assignment2_v1");
+$con = mysqli_connect("db4free.net","eitwebprog2019","Webprog2019","eitwebprog2019");
 
 if (mysqli_connect_errno())
   {
