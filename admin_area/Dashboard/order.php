bb7fdbbc90900a5db3b3e5b85040f563a3e33dc6
annavava2008 <49292021+annavava2008@users.noreply.github.com>
2019-04-13 14:05:40 +1200

    tried order page & sql queries

---
+++ admin_area/Dashboard/order.php
@@ -0,0 +1,13 @@
+<h2>View all Orders Here..</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>Order</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
