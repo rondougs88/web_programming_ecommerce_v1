b83f04d3fb549c3e07e305fe01465c2beeec367e
Ron <rondougs88@gmail.com>
2019-05-01 09:00:51 +1200

    testing edit order

--- shop_sidebar.php
+++ shop_sidebar.php
@@ -1,6 +1,18 @@
 <div class="col-lg-3">
 
-    <div class="list-group" style="margin-top:40px">
+    <div class="input-group" id="adv-search" style="margin-top:40px">
+        <input type="text" class="form-control" placeholder="Search for products" />
+        <div class="input-group-btn">
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-primary">
+                    <span class="fa fa-search" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <h6 class="my-4" style="margin-top:20px">See all products</h6>
+    <div class="list-group" style="margin-top:-16px">
         <a href='./all_products.php' class='list-group-item' id="all">All Products</a>
     </div>
 
